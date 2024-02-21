e5a24e582fd6556f9b198a31d58dfd0b866a4b92
dsrecec <86156532+dsrecec@users.noreply.github.com>
2024-02-21 14:41:00 +0100

    Generate new product component
    -Firstly, we created a new products component manually
    -After we learned how we can manually make component we used 'ng g c Products' to generate a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,15 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import {ProductsComponent} from "./products.component";
+import { ProductComponent } from './product/product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    ProductComponent,
+
   ],
   imports: [
     BrowserModule,
